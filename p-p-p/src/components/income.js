7afc1b938b69556d5fda86f490761a854f3465cc
naimad-adrec <106353663+naimad-adrec@users.noreply.github.com>
2023-05-18 14:24:18 -0700

    Added text changes

--- p-p-p/src/components/income.js
+++ p-p-p/src/components/income.js
@@ -7,19 +7,25 @@ const income = () => {
   return (
     <>
         <div className='income'>
-            <div className='content'>
-                <div><h1 className="title">Income</h1></div>
+            <div>
+                <div><h1 className="contentTitle">Income</h1></div>
                 <div className='budgetChart'>
                     <PieChart
                         data={[
-                            { title: 'Savings', value: 40, color: '#E38627' },
-                            { title: 'Food', value: 25, color: '#C13C37' },
-                            { title: 'Personal', value: 20, color: '#C13C37' },
-                            { title: 'Transportation', value: 5, color: '#C13C37' },
-                            { title: 'Medical', value: 5, color: '#C13C37' },
-                            { title: 'Housing', value: 5, color: '#C13C37' },
+                            { title: 'Savings', value: 40, color: '#bee7e8' },
+                            { title: 'Food', value: 25, color: '#a0d2db' },
+                            { title: 'Personal', value: 20, color: '#7d8cc4' },
+                            { title: 'Transportation', value: 5, color: '#726d88' },
+                            { title: 'Medical', value: 5, color: '#665780' },
+                            { title: 'Housing', value: 5, color: '#594157' },
                     ]}
                     />
+                    <p>Savings: 40%</p>
+                    <p>Food: 25%</p>
+                    <p>Personal: 20%</p>
+                    <p>Transportation: 5%</p>
+                    <p>Medical: 5%</p>
+                    <p>Housing: 5%</p>
                 </div>
             </div>
             <Banner></Banner>
